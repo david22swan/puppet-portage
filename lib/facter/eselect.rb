22b38e15886a24edf509afd5630d005cd74c1eb2
Robin H. Johnson <robbat2@gentoo.org>
2020-01-03 10:34:30 -0800

    lib/facter/eselect: refactor
    Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>

--- lib/facter/eselect.rb
+++ lib/facter/eselect.rb
@@ -1,5 +1,6 @@
 if Facter.value(:operatingsystem) == 'Gentoo'
-  eselect_modules = %x(eselect modules list).split('  ').reject! { |c| c.empty? }.delete_if { |c| c["\n"] }
+  ESELECT_CMD = '/usr/bin/eselect --brief --color=no'
+  eselect_modules = %x(#{ESELECT_CMD} modules list).split("\n").reject! { |c| c.chomp.empty? }.map { |s| s.split()[0] }
   eselect_modules_blacklist = [
     'help', 'usage', 'version', 'bashcomp', 'env', 'fontconfig', 'modules',
     'news', 'rc',
@@ -21,11 +22,11 @@ if Facter.value(:operatingsystem) == 'Gentoo'
   eselect_modules.each do |eselect_module|
     if (submodules = eselect_modules_multitarget[eselect_module])
       submodules.each do |target|
-        output = %x{eselect --brief --color=no #{eselect_module} show #{target}}.strip
+        output = %x{#{ESELECT_CMD} #{eselect_module} show #{target}}.strip
         facter_add("eselect_#{eselect_module}_#{target}", output)
       end
     else
-      output = %x{eselect --brief --color=no #{eselect_module} show}.strip.split(' ')[0]
+      output = %x{#{ESELECT_CMD} #{eselect_module} show}.strip.split(' ')[0]
       if not ['(none)', '(unset)'].include? output
         facter_add("eselect_#{eselect_module}", output)
       end
